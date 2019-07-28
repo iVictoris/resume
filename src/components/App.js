35387fb5ec6f2ef0e65bd3ce56df9c992789f75c
Victor Tran <victor.n.tran@outlook.com>
2019-07-28 15:59:04 -0700

    add view component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './navbar/navbar';
+import View from './view/view';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <View />
     </div>
   );
 }
