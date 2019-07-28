7c0ce9e9e18322a7999534250228380fcb922830
Victor Tran <victor.n.tran@outlook.com>
2019-07-28 16:04:04 -0700

    create educationExperience component

---
+++ src/components/educationExperience/educationExperience.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const EducationExperience = props => {
+  return (
+    <section className='education experience'>
+    <header>Education</header>
+      
+    </section>
+  )
+}
+
+EducationExperience.propTypes = {
+
+}
+
+export default EducationExperience
