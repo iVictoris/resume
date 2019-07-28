f6c81ee65f63849b74bd5cd7a15748291b9f9761
Victor Tran <victor.n.tran@outlook.com>
2019-07-28 16:02:15 -0700

    hook up About and workExperience components

--- src/components/view/view.js
+++ src/components/view/view.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import About from '../about/about';
+import WorkExperience from '../workExperience/workExperience';
+
+import './view.css';
 
 const View = props => {
   return (
-    <div>
+    <div className='view w80c'>
+      <About />
+      <WorkExperience />
       
     </div>
   )
