fb7c79ca84a094fd0affc11c5f3f97516f88e09f
Victor Tran <victor.n.tran@outlook.com>
2019-07-28 13:30:27 -0700

    testing out sidedrawer for future use case

---
+++ src/components/sidedrawer/sidedrawer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './sidedrawer.css';
+
+const Sidedrawer = props => {
+  return (
+    <div className='sidedrawer'>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+Sidedrawer.propTypes = {
+
+}
+
+export default Sidedrawer
