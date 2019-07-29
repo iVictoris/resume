2a2ff6e31af1a2e55659ccddc84b4d6022a3cb33
Victor Tran <victor.n.tran@outlook.com>
2019-07-28 19:43:03 -0700

    changed contact info and styled it

--- src/components/contactInfo/contactInfo.js
+++ src/components/contactInfo/contactInfo.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+
+import './contactInfo.css';
 
 const ContactInfo = props => {
   return (
-    <div>
-      
+    <div className="contact info">
+      <div>Victor Tran</div>
+      <div>6949 21st St N</div>
+      <div>Saint Petersburg, FL, 33702</div>
+      <div>(850) 866-5156</div>
+      <div>(727) 239-9316</div>
+      <div>victor.n.tran@outlook.com</div>
     </div>
-  )
-}
+  );
+};
 
-ContactInfo.propTypes = {
+ContactInfo.propTypes = {};
 
-}
-
-export default ContactInfo
+export default ContactInfo;
